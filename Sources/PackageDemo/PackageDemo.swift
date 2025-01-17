050603df0d92c8cc32ce1580d5bf8ace130975d4
Mahamud Siddiquee <mahamud.siddiquee@brainstation-23.com>
2025-01-17 17:37:43 +0600

    added dynamic view loader to the conforming view

--- Sources/PackageDemo/PackageDemo.swift
+++ Sources/PackageDemo/PackageDemo.swift
@@ -4,3 +4,32 @@
 //
 //  Created by Mahamud Siddiquee on 6/1/25.
 //
+
+
+import UIKit
+
+@MainActor
+class SDKInitializer {
+    
+    public static let shared = SDKInitializer()
+    
+    public static func attachView(
+        to viewController: UIViewController,
+        below targetView: UIView,
+        withHeight height: CGFloat
+    ) {
+        let customView = CustomUIView()
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        
+        viewController.view.addSubview(customView)
+
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: targetView.bottomAnchor),
+            customView.leadingAnchor.constraint(equalTo: targetView.leadingAnchor),
+            customView.trailingAnchor.constraint(equalTo: targetView.trailingAnchor),
+            customView.bottomAnchor.constraint(lessThanOrEqualTo: viewController.view.safeAreaLayoutGuide.bottomAnchor),
+            customView.heightAnchor.constraint(equalToConstant: 500)
+        ])
+        viewController.view.layoutIfNeeded()
+    }
+}
